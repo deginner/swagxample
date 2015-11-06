c7c2a78de1609e72203fb97866bd40d8f4a4cd46
isysd <ira@deginner.com>
2015-11-06 07:16:31 -0500

    heroku environment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gunicorn_app:gunicorn_app -b 0.0.0.0:5000 --log-file -
--- example_cfg.py
+++ example_cfg.py
@@ -1,4 +1,4 @@
-SA_ENGINE_URI = 'sqlite:////tmp/test.db'
+SA_ENGINE_URI = 'postgres:///swagxample'
 PRIV_KEY = "L4vB5fomsK8L95wQ7GFzvErYGht49JsCPJyJMHpB4xGM6xgi2jvG"
 PUB_KEY = "1F26pNMrywyZJdr22jErtKcjF8R3Ttt55G"
 
